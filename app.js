280a9e632f6eb9d402fe6c6cab213ebbf0801646
AlfredoOrtiz14 <40581292+AlfredoOrtiz14@users.noreply.github.com>
2020-03-27 13:00:58 -0600

    Galeria

--- app.js
+++ app.js
@@ -6,13 +6,50 @@ let p = 1;
 
 function cargaImagenes(e){
     e.preventDefault();
-    let algo = this.responseText;
+    let algo = e.target.innerHTML;
+
+    /*if(e.target.className === '')
+        let algo = e.target.parentElement;*/
     console.log(algo);
     while(document.getElementById("im"))
         document.getElementById("im").remove();
-    //p = document.getElementById("pag").value;
+    //p = document.getElementById("pag").value
+
+    const cantidad = document.getElementById('numero').value;
     
-    //console.log(JSON.parse(p));
+    let url = "";
+    url += "https://picsum.photos/v2/list";
+    if(cantidad !== '')
+    {
+        url += `?page=${algo}&`;
+        url += `limit=${cantidad}&`;
+    }
+
+    console.log(url);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.onload = function(){//Funcion que carga las imagenes
+        if(this.status === 200)
+        {
+            const imagenes = JSON.parse(this.responseText);
+            //console.log(imagenes);
+
+            let htmlImagen = '';
+            imagenes.forEach(function(imagen){
+                htmlImagen = '';
+                htmlImagen = '<div id="im" class="b-1 col-m-4 col-s-12">';
+                htmlImagen += `<img src="${imagen.download_url}" >`;
+                htmlImagen += '</div>'
+                //console.log(htmlImagen);
+                document.getElementById("Galeria").innerHTML += htmlImagen;
+                
+            });
+        }
+    }
+
+    xhr.send();
 }
 
 function cargarImagen(e){
@@ -47,7 +84,7 @@ function cargarImagen(e){
         urlBoton += `?page=${i+1}&`;
         urlBoton += `limit=${cantidad}&`
         let boton = '';
-        boton += `<li id="pag" class="b-pag" <a href="${urlBoton}"> `;
+        boton += `<li id="pag" class="b-pag" <a href="${urlBoton}">`;
         boton += `${i+1}</a></li>`;
         document.getElementById("Paginas").innerHTML += boton;
     }
@@ -57,7 +94,7 @@ function cargarImagen(e){
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
 
-    xhr.onload = function(){
+    xhr.onload = function(){//Funcion que carga las imagenes
         if(this.status === 200)
         {
             const imagenes = JSON.parse(this.responseText);
@@ -66,7 +103,7 @@ function cargarImagen(e){
             let htmlImagen = '';
             imagenes.forEach(function(imagen){
                 htmlImagen = '';
-                htmlImagen = '<div id="im" class="b-1 col-m-4">';
+                htmlImagen = '<div id="im" class="b-1 col-m-4 col-s-12">';
                 htmlImagen += `<img src="${imagen.download_url}" >`;
                 htmlImagen += '</div>'
                 //console.log(htmlImagen);
@@ -77,4 +114,5 @@ function cargarImagen(e){
     }
 
     xhr.send();
+    //document.querySelector('#pag').addEventListener('click', cargaImagenes);
 }
